c156eefb9e09dc014ab36bcfb1763749cc96902f
tsoim <26643911+tsoim@users.noreply.github.com>
2023-05-29 13:32:45 -0700

    Initialize training/lesson1

---
+++ src/main/java/frc/robot/HardwareMap.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+/**
+ * HardwareMap provides a centralized spot for constants related to the hardware
+ * configuration of the robot.
+ */
+public final class HardwareMap {
+	// ID numbers for devices on the CAN bus
+
+	// Pneumatics channel numbers
+
+}
